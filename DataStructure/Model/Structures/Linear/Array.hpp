91f63d3ea06d283491255cefedc6f1528b06d5eb
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-02-06 13:35:23 -0700

    Added Private and Public in class Array

--- DataStructure/Model/Structures/Linear/Array.hpp
+++ DataStructure/Model/Structures/Linear/Array.hpp
@@ -9,6 +9,27 @@
 #ifndef Array_hpp
 #define Array_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+template <class Type>
+class Array
+{
+private:
+    Type * internalArray;
+    int size;
+public:
+    Array<Type>(int size);
+    Array<Type>(const Array<Type> & toCopy);
+    ~Array<Type>();
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    int getSize() const;
+    Type getFromIndex(int index);
+    void setAtIndex(int index, Type data);
+};
 
 #endif /* Array_hpp */
