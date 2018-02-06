3fc425710d39bc0981b821c6071b5b0a9e16c519
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-02-06 12:36:27 -0700

    Added ArrayTesters and Array.hpp

---
+++ DataStructure/Model/Structures/Linear/Array.hpp
@@ -0,0 +1,14 @@
+//
+//  Array.hpp
+//  DataStructure
+//
+//  Created by Martinson, Branton on 2/6/18.
+//  Copyright © 2018 Martinson, Branton. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+#include <stdio.h>
+
+#endif /* Array_hpp */
---
+++ DataStructure/Testers/ArrayTester.cpp
@@ -0,0 +1,9 @@
+//
+//  ArrayTester.cpp
+//  DataStructure
+//
+//  Created by Martinson, Branton on 2/6/18.
+//  Copyright © 2018 Martinson, Branton. All rights reserved.
+//
+
+#include "ArrayTester.hpp"
