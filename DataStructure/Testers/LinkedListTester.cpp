20caba67f17a4fca2be86c4be8f204c86c8f089b
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-02-22 12:50:12 -0700

    Created LinkedListTester class and header class and added #includes into .cpp

---
+++ DataStructure/Testers/LinkedListTester.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedListTester.cpp
+//  DataStructure
+//
+//  Created by Martinson, Branton on 2/22/18.
+//  Copyright © 2018 Martinson, Branton. All rights reserved.
+//
+
+#include "LinkedListTester.hpp"
---
+++ DataStructure/Testers/LinkedListTester.hpp
@@ -0,0 +1,20 @@
+//
+//  LinkedListTester.hpp
+//  DataStructure
+//
+//  Created by Martinson, Branton on 2/22/18.
+//  Copyright © 2018 Martinson, Branton. All rights reserved.
+//
+
+#ifndef LinkedListTester_hpp
+#define LinkedListTester_hpp
+
+#include <iostream>
+#include "../Model/Structures/Node/Linear/List.hpp"
+#include "..Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+#include "../Data/CrimeData.hpp"
+
+
+
+#endif /* LinkedListTester_hpp */
