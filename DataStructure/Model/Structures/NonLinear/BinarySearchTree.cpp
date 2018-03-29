276aedc14c2f8d390c3a7846a90e8f71e5de9eee
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-03-29 13:16:45 -0600

    Created BinarySearchTree

---
+++ DataStructure/Model/Structures/NonLinear/BinarySearchTree.cpp
@@ -0,0 +1,9 @@
+//
+//  BinarySearchTree.cpp
+//  DataStructure
+//
+//  Created by Martinson, Branton on 3/29/18.
+//  Copyright © 2018 Martinson, Branton. All rights reserved.
+//
+
+#include "BinarySearchTree.hpp"
---
+++ DataStructure/Model/Structures/NonLinear/BinarySearchTree.hpp
@@ -0,0 +1,69 @@
+//
+//  BinarySearchTree.hpp
+//  DataStructure
+//
+//  Created by Martinson, Branton on 3/29/18.
+//  Copyright © 2018 Martinson, Branton. All rights reserved.
+//
+
+#ifndef BinarySearchTree_hpp
+#define BinarySearchTree_hpp
+
+#include <iostream>
+#include "tree.hpp"
+using namespace std;
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * startNode, int index, int size);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChildI(BinaryTreeNode<Type> * current);
+    
+    void removeNode(BinarTreeNode<Type> * removeMe);
+    
+public:
+    
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinaryTreeNode<Type> * getRoot();
+    void setRoot(BinaryTreeNode<Type> * root);
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    void demoTraversalSteps(BinaryTreeNode<Type> * node);
+    
+    int getSize();
+    int getHeight();
+    bool isComplete();
+    bool isBalanced();
+    
+    bool contains(Type value);
+    void insert(Type itemToInsert);
+    void remove(Type vaalue);
+    
+    type findMinimum();
+    Type findMaximum();
+};
+#endif /* BinarySearchTree_hpp */
+
+
+
+
+
+
+
+
+
